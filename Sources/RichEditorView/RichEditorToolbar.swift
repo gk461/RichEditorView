c1938e4372aee62b48e75eb41372459959d374d8
Gunavant Patel <gk@Vikrams-MacBook-Air.local>
2024-08-09 13:44:51 +0530

    Added Done option

--- Sources/RichEditorView/RichEditorToolbar.swift
+++ Sources/RichEditorView/RichEditorToolbar.swift
@@ -55,13 +55,6 @@ import UIKit
     }
 }
 
-
-public enum IconPlacement {
-    case left
-    case right
-}
-
-
 /// RichEditorToolbar is UIView that contains the toolbar for actions that can be performed on a RichEditorView
 @objcMembers open class RichEditorToolbar: UIView {
 
@@ -70,12 +63,6 @@ public enum IconPlacement {
 
     /// A reference to the RichEditorView that it should be performing actions on
     open weak var editor: RichEditorView?
-    
-    open var singleButtonPlacement: IconPlacement = .right {
-        didSet {
-            updateToolbar()
-        }
-    }
     
     /// The list of options to be displayed on the toolbar
     open var options: [RichEditorOption] = [] {
@@ -93,6 +80,7 @@ public enum IconPlacement {
     private var toolbarScroll: UIScrollView
     private var toolbar: UIToolbar
     private var backgroundToolbar: UIToolbar
+    private var doneToolbar: UIToolbar
     
     public override init(frame: CGRect) {
         toolbarScroll = UIScrollView()
@@ -121,6 +109,11 @@ public enum IconPlacement {
         toolbar.backgroundColor = .clear
         toolbar.setBackgroundImage(UIImage(), forToolbarPosition: .any, barMetrics: .default)
         toolbar.setShadowImage(UIImage(), forToolbarPosition: .any)
+        
+        doneToolbar.autoresizingMask = .flexibleWidth
+        doneToolbar.backgroundColor = .clear
+        doneToolbar.setBackgroundImage(UIImage(), forToolbarPosition: .any, barMetrics: .default)
+        doneToolbar.setShadowImage(UIImage(), forToolbarPosition: .any)
 
         toolbarScroll.frame = bounds
         toolbarScroll.autoresizingMask = [.flexibleHeight, .flexibleWidth]
@@ -132,70 +125,55 @@ public enum IconPlacement {
 
         addSubview(backgroundToolbar)
         addSubview(toolbarScroll)
+        addSubView(doneToolbar)
         updateToolbar()
     }
     
     private func updateToolbar() {
         var buttons = [UIBarButtonItem]()
         
+        let doneOption = options.first(where: { $0.title == RichEditorDefaultOption.done.title})
+        options.removeAll(where: { $0.title == RichEditorDefaultOption.done.title})
         
-        if options.count == 1 {
-            let flexSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
-            buttons.append(flexSpace)
-            for option in options {
-                let handler = { [weak self] in
-                    if let strongSelf = self {
-                        option.action(strongSelf)
-                    }
-                }
-                if let image = option.image {
-                    let button = RichBarButtonItem(image: image, handler: handler)
-                    buttons.append(button)
-                } else {
-                    let title = option.title
-                    let button = RichBarButtonItem(title: title, handler: handler)
-                    buttons.append(button)
-                }
-            }
-            toolbar.items = buttons
-        } else {
-            
-            for option in options {
-                let handler = { [weak self] in
-                    if let strongSelf = self {
-                        option.action(strongSelf)
-                    }
-                }
-
-                if let image = option.image {
-                    let button = RichBarButtonItem(image: image, handler: handler)
-                    buttons.append(button)
-                } else {
-                    let title = option.title
-                    let button = RichBarButtonItem(title: title, handler: handler)
-                    buttons.append(button)
-                }
-            }
-            toolbar.items = buttons
-
-            let defaultIconWidth: CGFloat = 28
-            let barButtonItemMargin: CGFloat = 12
-            let width: CGFloat = buttons.reduce(0) {sofar, new in
-                if let view = new.value(forKey: "view") as? UIView {
-                    return sofar + view.frame.size.width + barButtonItemMargin
-                } else {
-                    return sofar + (defaultIconWidth + barButtonItemMargin)
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+        
+        for option in options {
+            let handler = { [weak self] in
+                if let strongSelf = self {
+                    option.action(strongSelf)
                 }
             }
             
-            if width < frame.size.width {
-                toolbar.frame.size.width = frame.size.width + barButtonItemMargin
+            if let image = option.image {
+                let button = RichBarButtonItem(image: image, handler: handler)
+                buttons.append(button)
             } else {
-                toolbar.frame.size.width = width + barButtonItemMargin
+                let title = option.title
+                let button = RichBarButtonItem(title: title, handler: handler)
+                buttons.append(button)
             }
-            toolbar.frame.size.height = 44
-            toolbarScroll.contentSize.width = width
         }
+        toolbar.items = buttons
+        
+        doneToolbar.items = [doneOption]
+        
+        let defaultIconWidth: CGFloat = 28
+        let barButtonItemMargin: CGFloat = 12
+        let width: CGFloat = buttons.reduce(0) {sofar, new in
+            if let view = new.value(forKey: "view") as? UIView {
+                return sofar + view.frame.size.width + barButtonItemMargin
+            } else {
+                return sofar + (defaultIconWidth + barButtonItemMargin)
+            }
+        }
+        
+        if width < frame.size.width {
+            toolbar.frame.size.width = frame.size.width + barButtonItemMargin
+        } else {
+            toolbar.frame.size.width = width + barButtonItemMargin
+        }
+        toolbar.frame.size.height = 44
+        toolbarScroll.contentSize.width = width
         
     }
     
